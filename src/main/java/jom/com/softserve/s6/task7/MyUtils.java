d7707db9715f7a65847225d523eb8dd29bc194f2
Kostya Kravchenko <kostiantyn_kravchenko@epam.com>
2024-08-18 17:15:17 +0300

    init

--- src/main/java/jom/com/softserve/s6/task7/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task7/MyUtils.java
@@ -1,13 +1,24 @@
 package jom.com.softserve.s6.task7;
 
-import java.util.Collection;
+import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class MyUtils {
 
-    //Write your code here
-
+    public static Stream<Integer> duplicateElements(Stream<Integer> stream) {
+        if (stream == null) {
+            throw new NullPointerException("Input stream cannot be null");
+        }
+        return stream.filter(Objects::nonNull)
+                .collect(groupingBy(identity(), counting()))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() > 1)
+                .map(Map.Entry::getKey)
+                .sorted();
+    }
 }
