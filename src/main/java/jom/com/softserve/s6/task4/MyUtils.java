c47113f081ea6def96d9db36026c68c188859ae0
Kostya Kravchenko <kostiantyn_kravchenko@epam.com>
2024-08-18 15:59:44 +0300

    init

--- src/main/java/jom/com/softserve/s6/task4/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task4/MyUtils.java
@@ -1,13 +1,17 @@
 package jom.com.softserve.s6.task4;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.Locale;
 
 public class MyUtils {
-
-    //Write your code here
-
+    public static String readFile(String filename) {
+        try {
+            String bData = java.nio.file.Files.readString(java.nio.file.Paths.get(filename)).replaceAll("[\\[\\], ]", "");
+            return java.util.stream.IntStream.range(0, bData.length() / 7)
+                    .mapToObj(i -> bData.substring(i * 7, (i + 1) * 7))
+                    .map(binary -> String.valueOf((char) Integer.parseInt(binary, 2)))
+                    .collect(java.util.stream.Collectors.joining());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
