d19afc0ea025e836675c6bcc198636dcae32ae67
Kostya Kravchenko <kostiantyn_kravchenko@epam.com>
2024-08-18 14:08:19 +0300

    init

--- src/main/java/jom/com/softserve/s6/task1/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task1/MyUtils.java
@@ -1,9 +1,13 @@
 package jom.com.softserve.s6.task1;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 public class MyUtils {
 
-    //Write your code here
+    public static boolean isLeapYear(int year) {
+//    return (year & 3) == 0 && ((year % 100) != 0 || (year % 400) == 0);
+        return LocalDate.of(year, Month.JANUARY, 1).isLeapYear();
+    }
 
 }
