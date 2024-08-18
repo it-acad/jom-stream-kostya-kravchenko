9f2e00b94811be5be648f9c8f907b7d72399eeb0
Kostya Kravchenko <kostiantyn_kravchenko@epam.com>
2024-08-18 18:27:52 +0300

    init

--- src/main/java/jom/com/softserve/s6/task2/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task2/MyUtils.java
@@ -1,12 +1,10 @@
 package jom.com.softserve.s6.task2;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
+import java.time.Period;
 
 public class MyUtils {
     public static String getDateAfterToday(int years, int months, int days) {
-        //Write code there
-        return "";
+        return LocalDate.now().plus(Period.of(years,months,days)).toString();
     }
 }
