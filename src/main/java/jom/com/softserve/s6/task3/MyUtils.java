4cfde552618c4647ad09578f378a138633063383
Kostya Kravchenko <kostiantyn_kravchenko@epam.com>
2024-08-18 15:44:56 +0300

    init

--- src/main/java/jom/com/softserve/s6/task3/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task3/MyUtils.java
@@ -1,11 +1,22 @@
 package jom.com.softserve.s6.task3;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 public class MyUtils {
 
-    //Write your code here
+    public static void writeFile(String filename, String text) {
+        try {
+            String output = text.chars()
+                    .mapToObj(c -> "%7s".formatted(Integer.toBinaryString(c)).replace(' ', '0'))
+                    .collect(java.util.stream.Collectors.joining());
+            java.nio.file.Files.write(java.nio.file.Paths.get(filename), output.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        writeFile("data1.txt", "Hello!");
+    }
 
 }
