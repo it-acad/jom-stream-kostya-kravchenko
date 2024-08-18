379ccbdc76a25d9cfdc65bb3ef9d4a4f568f5ae7
Kostya Kravchenko <kostiantyn_kravchenko@epam.com>
2024-08-18 16:14:05 +0300

    init

--- src/main/java/jom/com/softserve/s6/task5/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task5/MyUtils.java
@@ -7,6 +7,18 @@ import java.util.stream.Stream;
 
 public class MyUtils {
 
-    //Write your code here
-
+    public Stream<String> nameList(Map<String, Stream<String>> map) {
+        if (Objects.isNull(map)) {
+            throw new NullPointerException();
+        }
+        return map.values().stream()   // Stream<Stream<String>>
+                .flatMap(Function.identity())  // Flatten to Stream<String>
+                .filter(Objects::nonNull)   // Filter out nulls
+                .map(name -> name.replaceAll("\\s+", ""))          // Trim leading/trailing spaces
+                .filter(name -> !name.isEmpty())  // Filter out empty strings
+                .map(String::toLowerCase)   // Normalize case for uniqueness
+                .distinct()                 // Remove duplicates
+                .sorted()                   // Sort alphabetically
+                .map(name -> Character.toUpperCase(name.charAt(0)) + name.substring(1));
+    }
 }
